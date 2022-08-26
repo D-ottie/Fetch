84a7caf4c390c063de72444beaee5b8e9afccce2
Dottie77 <dorothyorina7@gmail.com>
2022-08-26 12:05:14 +0300

    Complete game _ Initial commit
    Send your dog to fetch the balls before they fall to the ground

---
+++ Assets/Challenge 2/Scripts/DetectCollisionsX.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisionsX : MonoBehaviour
+{
+    public GameObject ball1;
+    public GameObject ball2;
+    public GameObject ball3;
+    public int counter;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+    }
+}
